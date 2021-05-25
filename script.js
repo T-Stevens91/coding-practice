f211e370e84852a99d25ba7f5b5e088af9a69f9b
T Stevens <tom_stevens91@hotmail.com>
2021-05-25 18:21:29 +0100

    debugged and working

--- script.js
+++ script.js
@@ -2,22 +2,26 @@ const meals = {
 	breakfast: ['cereal', 'toast', 'fry-up'],
 	lunch: ['sandwiches', 'wraps', 'salad'],
 	dinner: ['pizza', 'steak', 'pasta']
-}
+};
 
-const generateRandNum = () => Math.floor(Math.random * 3);
+function generateRandNum(){
+	return Math.floor(Math.random() * 3);
+};
+
 let returnedMeals = [];
 
 for (let meal in meals) {
+	let generatedIndex = generateRandNum();
 	switch(meal){
 		case 'breakfast':
-			returnedMeals.push(`For breakfast we have ${meals[meal][generateRandNum()]},`);
-			break;
+			returnedMeals.push(`For breakfast we have ${meals[meal][generatedIndex]},`);
+			break
 		case 'lunch':
-			returnedMeals.push(`For lunch we have ${meals[meal][generateRandNum()]},`);
-			break;
+			returnedMeals.push(`For lunch we have ${meals[meal][generatedIndex]},`);
+			break
 		case 'dinner':
-			returnedMeals.push(`And for dinner we have ${meals[meal][generateRandNum()]}.`);
-			break;
+			returnedMeals.push(`And for dinner we have ${meals[meal][generatedIndex]}.`);
+			break
 		default:
 			returnedMeals.push('There has been an error');
 	}

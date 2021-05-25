4e19d13f8efdc3849e1019a3330f71d7a8e12259
T Stevens <tom_stevens91@hotmail.com>
2021-05-25 17:19:35 +0100

    create array for returned values and loop to generate random text

--- script.js
+++ script.js
@@ -5,4 +5,20 @@ const meals = {
 }
 
 const generateRandNum = () => Math.floor(Math.random * 3);
+let returnedMeals = [];
 
+for (let meal in meals) {
+	switch(meal){
+		case 'breakfast':
+			returnedMeals.push(`For breakfast we have ${meals[meal][generateRandNum()]},`);
+			break;
+		case 'lunch':
+			returnedMeals.push(`For lunch we have ${meals[meal][generateRandNum()]},`);
+			break;
+		case 'dinner':
+			returnedMeals.push(`And for dinner we have ${meals[meal][generateRandNum()]}.`);
+			break;
+		default:
+			returnedMeals.push('There has been an error');
+	}
+}
